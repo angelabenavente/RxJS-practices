52619ed2223e2adc278ba3999c347fea2e6529b9
angelabenavente <angela.b.almazan@gmail.com>
2020-02-15 22:50:09 +0100

    Use from function

--- src/create.js
+++ src/create.js
@@ -1,19 +1,39 @@
 import { displayLog } from './utils';
-import { Observable } from 'rxjs';
+//import { Observable } from 'rxjs';
+import { from } from 'rxjs';
 
 export default () => {
-    /** start coding */
+	/*
+	// Create observer
+	const hello = Observable.create(function(observer){
+		observer.next("Hello");
+		setTimeout(()=>{
+			observer.next("World");
+			observer.complete();
+		}, 2000);
+	});
 
-    const hello = Observable.create(function(observer){
-        observer.next("Hello");
-        setTimeout(()=>{
-            observer.next("World");
-        }, 2000);
-    });
+	const observer = {
+		next: event => displayLog(event),
+		error: err => console.err("[ERR]", err),
+		complete: () => displayLog("[DONE]")
+	}
 
-    const subscribe = hello.subscribe(event => displayLog(event));
+	const subscribe = hello.subscribe(observer);
+	const subscribe2 = hello.subscribe(observer);
+	subscribe.unsubscribe();
+	*/
 
+	// From function
 
+	const myArray = [1,2,3,4,5];
+	const myString = "Helo World"
+	const myPromise = new Promise(resolve => setTimeout(() => {
+		resolve("Hello World");
+	}, 2000));
 
-    /** end coding */
+	// const observable = from(myArray);
+	// const observable = from(myString);
+	const observable = from(myPromise);
+	const subscription = observable.subscribe(val => displayLog(val));
 }
