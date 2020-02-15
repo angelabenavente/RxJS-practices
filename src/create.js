2c2da60376ce2fbb83cf2ab3c61f516852b874b4
angelabenavente <angela.b.almazan@gmail.com>
2020-02-15 22:13:48 +0100

    Add new structure

---
+++ src/create.js
@@ -0,0 +1,19 @@
+import { displayLog } from './utils';
+import { Observable } from 'rxjs';
+
+export default () => {
+    /** start coding */
+
+    const hello = Observable.create(function(observer){
+        observer.next("Hello");
+        setTimeout(()=>{
+            observer.next("World");
+        }, 2000);
+    });
+
+    const subscribe = hello.subscribe(event => displayLog(event));
+
+
+
+    /** end coding */
+}
