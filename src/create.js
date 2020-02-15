e94d717ffd6ff437f82639ab6adce2487ca3f78a
angelabenavente <angela.b.almazan@gmail.com>
2020-02-15 23:40:51 +0100

    Use interval & timer functions

--- src/create.js
+++ src/create.js
@@ -1,7 +1,9 @@
 import { displayLog } from './utils';
 // import { Observable } from 'rxjs';
 // import { from } from 'rxjs';
-import { of, range } from 'rxjs';
+// import { of, range } from 'rxjs';
+//import { Subscription } from 'rxjs/internal/Subscription';
+import { interval, timer } from 'rxjs';
 
 export default () => {
 	/*
@@ -40,8 +42,9 @@ export default () => {
 	const subscription = observable.subscribe(val => displayLog(val));
 	*/
 
+	/*
 	// Of & range functions
-	
+
 	const source = of(1,2,3,4,5,6);
 	const source2 = of(
 		[1,2,3],
@@ -55,5 +58,19 @@ export default () => {
 	const source3 = range(3,10); // Number range starts at 3 and it has 10 numbers
 
 	const subscription = source3.subscribe( data => displayLog(data));
+	*/
 
+	// Interval & timer functions
+
+	const source = interval(500);
+	//interval return a infinite sequence. It need finish subcription 
+	const subscription = source.subscribe(data => displayLog(data));
+
+	timer(3000).subscribe(() => subscription.unsubscribe())
+	// setTimeout(() => subscription.unsubscribe(), 3000);
+
+	const source2 = timer(4000, 100); // start at 4000ms and work for 100ms
+	const subscription2 = source2.subscribe(data => displayLog(`2 - ${data}`));
+
+	timer(6000).subscribe(() => subscription2.unsubscribe());
 }
